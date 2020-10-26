79b2a6f6a4733b47031821aa7ce5aa55766b9eb8
Rheydskey <matgag02@gmail.com>
2020-10-26 10:16:54 +0100

    Initial Cli command commit

--- src/lib/cli/cli.rs
+++ src/lib/cli/cli.rs
@@ -1,49 +1,22 @@
-use clap::{App, Arg, SubCommand};
 
-pub fn cli() {
-    let matches = App::new("Sulfurium OS package Manager")
-        .version("0.1.0")
-        .author("Rheydskey")
-        .about("This is the package manager of Sulfurium Os fast and Rusted Distro")
-        //.arg(Arg::with_name("Action").required(true))
-        .arg(Arg::with_name("install")
-            .short("i")
-            .long("install")
-            .value_name("Package")
-            .help("Install a package")
-            .takes_value(true))
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .help("Sets the level of verbosity"))
-        .subcommand(SubCommand::with_name("test")
-            .about("controls testing features")
-            .version("1.3")
-            .author("Someone E. <someone_else@other.com>")
-            .arg(Arg::with_name("debug")
-                .short("d")
-                .help("print debug information verbosely")))
-        .get_matches();
+use structopt::StructOpt;
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("Action").unwrap());
-
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
-        }
-    }
-
+#[derive(StructOpt, Debug)]
+#[structopt(name = "Sulfur -", version = "0.0.1", author = "By Rheydskey and Sulfurium OS team", about = "Package Manager of Sulfurium OS")]
+pub enum Cli {
+    /// Install a package
+    Install {
+        #[structopt()]
+        package_name: Vec<String>,
+    },
+    /// Remove a package
+    Remove {
+        #[structopt()]
+        package_name: Vec<String>,
+    },
+    /// Search a package
+    Query {
+        #[structopt()]
+        package_name: Vec<String>,
+    },
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use lib::cli::cli::cli;
+use lib::cli::cli::Cli;
+use structopt::StructOpt;
 
 mod lib;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
     if users::get_current_uid() == 0 {
-        cli()
+        let opt = Cli::from_args();
+        println!("{:#?}", opt);
     } else {
         print!("You not are in root ! Please rerun this command with root account or sudo");
     }
