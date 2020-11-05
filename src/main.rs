248505a7f91a617ac55acbef95562cafd6239f51
Rheydskey <matgag02@gmail.com>
2020-11-05 13:04:13 +0100

    Fix autoprint Ok

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 use crate::lib::default::default::default;
 use crate::lib::packages::install::install;
-use lib::cli::cli::Cli;
+use lib::{cli::cli::Cli, packages::{query::query, remove::remove}};
 use structopt::StructOpt;
 
 mod lib;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main(){
 
     default().await.expect("Error");
 
     if users::get_current_uid() == 0 {
         match Cli::from_args() {
             Cli::Install { packages } => install(packages).await,
-            Cli::Query { packages } => println!("{:?}", packages),
-            Cli::Remove { packages } => println!("{:?}", packages),
+            Cli::Query { packages } => query(packages).await,
+            Cli::Remove { packages } => remove(packages),
         };
     } else {
         print!("You not are in root ! Please rerun this command with root account or sudo");
     }
-
-    Ok(())
 }
