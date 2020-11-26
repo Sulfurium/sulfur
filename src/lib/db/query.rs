e38df55456cf6490e441df5c35a51a9df5392fab
Rheydskey <matgag02@gmail.com>
2020-11-26 18:42:09 +0100

    :sparkles: retrieving all the data table

--- src/lib/db/query.rs
+++ src/lib/db/query.rs
@@ -17,7 +17,7 @@ pub async fn query_package(package: String) -> std::io::Result<Vec<PKG>> {
 
     while let Some(row) = rows.try_next().await.expect("Error") {
         let mut pkg = PKG::new();
-
+        pkg.set_id(row.try_get::<i64, &str>("id").expect("Error"));
         pkg.set_name(
             row.try_get::<&str, &str>("name")
                 .expect("Error")
@@ -67,9 +67,26 @@ pub async fn query_package(package: String) -> std::io::Result<Vec<PKG>> {
 
         pkg.set_installed_from_i64(row.try_get::<i64, &str>("installed").expect("Error"))
             .expect("Error");
-
+        
+        pkg.set_files(string_to_vec(row.try_get::<&str, &str>("file").expect("Error").to_string()));
+        pkg.set_dependence(string_to_vec(row.try_get::<&str, &str>("dependence").expect("Error").to_string()));
+        pkg.set_optional_dependence(string_to_vec(row.try_get::<&str, &str>("optional_dependence").expect("Error").to_string()));
+        
         vec_pkg.push(pkg);
     }
 
     Ok(vec_pkg)
+}
+
+pub fn string_to_vec(string: String) -> Vec<String> {
+    let string_replace = string.replace("[", "").replace("]", "");
+    let mut result: Vec<String> = Vec::new();
+    let split: Vec<&str> = string_replace.split(",").collect();
+
+    for v in split {
+        result.push(v.to_string());
+    };
+
+    result
+
 }
